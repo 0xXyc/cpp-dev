3d129944542f9c9a46fcb7c8cf45b6fdffe162e4
unknown <nope@gmail.com>
2023-06-22 22:47:05 -0400

    First challenge completed

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -2,6 +2,13 @@
 
 int main()
 {
-    std::cout << "Hello world!\nThis is my first file in c++!";
+    int a = 1;          // Original variables -- Write code to swap the value of two variables
+    int b = 2;
+                       // Swap here
+    int temp = a;
+    a = b;
+    b = temp;
+    std::cout << a;
+
     return 0;
 }
